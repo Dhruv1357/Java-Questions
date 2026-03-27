695aeb98c11336ae681afeb12a18192411859c17
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-03-27 08:37:39 +0530

    Changing the print message of toString

---
+++ Changing_toString.java
@@ -0,0 +1,15 @@
+    class One
+{
+   public String toString()
+    {
+        return "Hello how are you?";
+    }
+}
+public class Changing_toString
+{
+    public static void main(String[] args)
+    {
+        One object = new One();
+        System.out.println(object);
+    }
+}
