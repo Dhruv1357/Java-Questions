a8ad5de2e8ad559b495825e61fa1de95547b1674
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-04-06 22:49:03 +0530

    Trying different things on Static variable

---
+++ StaticVariable.java
@@ -0,0 +1,24 @@
+class Static
+{
+    static int value = 10;
+    int a;
+
+}
+public class StaticVariable 
+{
+    public static void main(String[] args)
+    {
+        Static s1 = new Static();
+        Static s2 = new Static();
+
+        s1.value = 5;
+
+        s1.a = 2;
+        s2.a = 4;
+
+        System.out.println(s2.value);
+        // System.out.println(s1.a);
+        // System.out.println(s2.a);
+
+    }
+}
