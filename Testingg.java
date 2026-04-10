ad4f5a66d48f4d6b02d07116d459d125f0ba5c1c
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-04-10 22:50:47 +0530

    Taking and printing the marks of students

--- Testingg.java
+++ Testingg.java
@@ -1,9 +1,9 @@
+
 import java.util.Scanner;
 
-public class Testingg
-{
-    public static void main(String[] args) 
-    {
+public class Testingg {
+
+    public static void main(String[] args) {
         int size;
         System.out.println("Enter the number of students:");
         Scanner scan = new Scanner(System.in);
@@ -22,18 +22,15 @@ public class Testingg
         Scanner marks2 = new Scanner(System.in);
         marks[2] = marks2.nextInt();
 
-
         System.out.println("Enter the marks of Student 4: ");
         Scanner marks3 = new Scanner(System.in);
         marks[3] = marks3.nextInt();
-
 
         System.out.println("Enter the marks of Student 5: ");
         Scanner marks4 = new Scanner(System.in);
         marks[4] = marks4.nextInt();
 
-        System.out.println("The marks of the students are: "+ marks[0] +" "+ marks[1] +" "+ marks[2] +" "+ marks[3] +" "+ marks[4]);
+        System.out.println("The marks of the students are: " + marks[0] + " " + marks[1] + " " + marks[2] + " " + marks[3] + " " + marks[4]);
 
-
-    }    
+    }
 }
