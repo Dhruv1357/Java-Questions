2a687edcdc4d3533409312ba34e466d53168718c
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-04-19 22:48:40 +0530

    Changing the print message for object printing

--- Changing_toString.java
+++ Changing_toString.java
@@ -9,9 +9,10 @@ public class Changing_toString
 {
     public static void main(String[] args)
     {
-        
+
         One object = new One();
 
         System.out.println(object);
+        
     }
 }
