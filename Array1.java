692f324345ddbc6917eaebe205f23ce1a3f28cff
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-04-22 22:55:45 +0530

    1-D array

--- Array1.java
+++ Array1.java
@@ -9,11 +9,12 @@ public class Array1
         System.out.println("Enter the number of students: ");
         Scanner scan = new Scanner(System.in);
         int size = scan.nextInt();
-        
+
         int[] a = new int[size];
 
         System.out.println("Enter the marks of students: ");
         Scanner scan2 = new Scanner(System.in);
+        
         for (int i = 0; i < size; i++) 
         {
             a[i] = scan2.nextInt();
