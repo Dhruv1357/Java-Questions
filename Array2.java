12126f8de67354a52917939c7ccfed81c4ab5f3e
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-04-23 22:20:10 +0530

    2-D Array

---
+++ Array2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Array2 
+{
+    public static void main(String[] args) 
+    {
+        int students,classrooms;
+        System.out.println("Enter the number of students and classrooms: ");
+        Scanner scan1 = new Scanner(System.in);
+        students = scan1.nextInt();
+        classrooms = scan1.nextInt();
+        int[][] a = new int[classrooms][students];
+
+        System.out.println("Enter the marks of students: ");
+        Scanner scan2 = new Scanner(System.in);
+
+        
+        for(int i=0;i<classrooms;i++)
+        {
+            for(int j=0;j<students;j++)
+            {
+                a[i][j] = scan2.nextInt();
+            }
+        }
+
+        for(int i=0;i<classrooms;i++)
+        {
+            for(int j=0;j<students;j++)
+            {
+                System.out.println("The marks of student "+(j+1)+" of classroom "+(i+1)+": "+a[i][j]);   
+            }
+        }
+    }
+    
+}
