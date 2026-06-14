c7fed0c71e34008c81c06587937e080a64b94c82
Dhruv1357 <ds.dhruvsharma.2005@gmail.com>
2026-06-14 23:13:25 +0530

    Operations using dimensions

--- Main.java
+++ Main.java
@@ -51,13 +51,14 @@ class Rectangle extends Shape
 class Circle extends Shape
 {
     float radius;
+    
     public void acceptInput()
     {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter the radius of the circle: ");
         radius = scan.nextFloat();
     }
-    
+
     public void compute()
     {
         area = 3.14f * radius * radius;
